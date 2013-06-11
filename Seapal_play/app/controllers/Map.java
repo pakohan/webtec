dd2eb8ec460e6fa64220c0bb692320389ee592b3
Tobias Keh <tobias.keh@googlemail.com>
2013-06-11 14:58:15 +0200

    controller extended

--- Seapal_play/app/controllers/Map.java
+++ Seapal_play/app/controllers/Map.java
@@ -9,7 +9,7 @@ import views.html._include.*;
 public class Map extends Controller {
   
   public static Result index() {
-    return ok(map.render(header.render(), header_app.render(), navigation.render("app_map"), navigation_app.render("app_map")));
+    return ok(map.render(header.render(), header_app.render(), navigation.render("app_map"), navigation_app.render("app_map"), map_content.render()));
   }
   
 }
