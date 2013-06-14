3695aa0f270b0678af29dc70e4d10f8329e7c34a
Tobias Keh <tobias.keh@googlemail.com>
2013-06-14 21:03:36 +0200

    test

--- Seapal_play/public/javascripts/app/route/route_map.js
+++ Seapal_play/public/javascripts/app/route/route_map.js
@@ -1,34 +1,50 @@
 $(function() {
 
-	$('a.view').click(
-			alert("richtig");
-			route = new google.maps.Polyline(activePathOptions);
-			currentMode = MODE.ROUTE;
-			currentRoute = new Route(route);
-			
-			
-			document.getElementById("route_info_table").style.display = 'block';
-			document.getElementById("map_canvas").style.visibility = 'visible';
-			
-			//TODO: should get coordinates from server
+	$('a.view')
+			.click(
 
-			var tripCoordinates = [
-					new google.maps.LatLng(47.655733, 9.206272),
-					new google.maps.LatLng(47.656196, 9.223953),
-					new google.maps.LatLng(47.656080, 9.254681),
-					new google.maps.LatLng(47.656369, 9.288326),
-					new google.maps.LatLng(47.521780, 9.719282) ];
+					function printTrip(coordinates) {
 
-			addRouteMarker(tripCoordinates[0]);
-			addRouteMarker(tripCoordinates[1]);
-			addRouteMarker(tripCoordinates[2]);
-			addRouteMarker(tripCoordinates[3]);
-			addRouteMarker(tripCoordinates[4]);
+						document.getElementById("route_info_table").style.display = 'block';
+						document.getElementById("map_canvas").style.visibility = 'visible';
 
-			tripPath.setMap(map);
-			map.setCenter(tripCoordinates[0]);
-			
-			route.setMap(map);
-			routeArray.push(currentRoute);
+						// TODO: should get coordinates from server
 
+						var tripCoordinates = [
+								new google.maps.LatLng(47.655733, 9.206272),
+								new google.maps.LatLng(47.656196, 9.223953),
+								new google.maps.LatLng(47.656080, 9.254681),
+								new google.maps.LatLng(47.656369, 9.288326),
+								new google.maps.LatLng(47.521780, 9.719282) ];
+
+						var tripPath = new google.maps.Polyline({
+							path : tripCoordinates,
+							strokeColor : '#FF0000',
+							strokeOpacity : 1.0,
+							strokeWeight : 2,
+							draggable: true
+						});
+
+						for (x in tripCoordinates) {
+
+							marker = new google.maps.Marker({
+								position : tripCoordinates[x],
+								map : map
+							});
+
+							google.maps.event.addListener(marker, 'click',
+									function() {
+										var lat = this.getPosition().lat();
+										var lng = this.getPosition().lng();
+
+										map.setCenter(new google.maps.LatLng(
+												lat, lng));
+										getHistoricWeatherById(lat, lng);
+									});
+						}
+
+						tripPath.setMap(map);
+						map.setCenter(tripCoordinates[0]);
+
+					});
 });
