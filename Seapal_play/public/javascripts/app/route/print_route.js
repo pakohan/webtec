35e6920bb3d74b5d515bf049664fbe38df977b9c
Tobias Keh <tobias.keh@googlemail.com>
2013-06-15 16:28:37 +0200

    draggable crap deleted and better context menu implemented :-)

--- Seapal_play/public/javascripts/app/route/print_route.js
+++ Seapal_play/public/javascripts/app/route/print_route.js
@@ -1,46 +1,42 @@
 $(function() {
 
-	$('a.view').click(
-			
-			function printRoute(coordinates) {
-				
-				document.getElementById("route_info_table").style.display = 'block';
-				document.getElementById("map_canvas").style.visibility = 'visible';
-				
-				//TODO: should get coordinates from server
+	$('a.view')
+			.click(
 
-				var tripCoordinates = [
-						new google.maps.LatLng(47.655733, 9.206272),
-						new google.maps.LatLng(47.656196, 9.223953),
-						new google.maps.LatLng(47.656080, 9.254681),
-						new google.maps.LatLng(47.656369, 9.288326),
-						new google.maps.LatLng(47.521780, 9.719282) ];
+					function printTrip(coordinates) {
 
-				var tripPath = new google.maps.Polyline({
-					path : tripCoordinates,
-					strokeColor : '#FF0000',
-					strokeOpacity : 1.0,
-					strokeWeight : 2
-				});
+						document.getElementById("route_info_table").style.display = 'block';
+						document.getElementById("map_canvas").style.visibility = 'visible';
+						document.getElementById("markers").style.visibility = 'visible';
 
-				for (x in tripCoordinates) {
+						// TODO: should get coordinates from server
 
-					marker = new google.maps.Marker({
-						position : tripCoordinates[x],
-						map : map,
-						draggable: true
+						var tripCoordinates = [
+								new google.maps.LatLng(47.655733, 9.206272),
+								new google.maps.LatLng(47.656196, 9.223953),
+								new google.maps.LatLng(47.656080, 9.254681),
+								new google.maps.LatLng(47.656369, 9.288326),
+								new google.maps.LatLng(47.521780, 9.719282) ];
+
+						var tripPath = new google.maps.Polyline({
+							path : tripCoordinates,
+							strokeColor : '#FF0000',
+							strokeOpacity : 1.0,
+							strokeWeight : 2,
+							editable : true
+						});
+
+						google.maps.event.addListener(tripPath, "mouseup", getPath);
+
+						function getPath() {
+							var path = tripPath.getPath();
+							var len = path.getLength();
+							alert("store route");
+						}
+
+						tripPath.setMap(map);
+						map.setCenter(tripCoordinates[0]);
+
 					});
-
-					google.maps.event.addListener(marker, 'click', function() {
-						var lat = this.getPosition().lat();
-						var lng = this.getPosition().lng();
-
-						map.setCenter(new google.maps.LatLng(lat, lng));
-					});
-				}
-
-				tripPath.setMap(map);
-				map.setCenter(tripCoordinates[0]);
-			});
 
 });
